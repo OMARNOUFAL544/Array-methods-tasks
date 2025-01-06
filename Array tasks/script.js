8c5660ea5d42e2094b6cd09bdcb1b70d0c77248e
OMARNOUFAL544 <omarnofal544@gmail.com>
2025-01-07 00:01:20 +0300

    Initial commit

---
+++ Array tasks/script.js
@@ -0,0 +1,147 @@
+
+   
+   
+        //  task 1
+
+
+// const array = [1, 2, 3, 4, 5];
+// console.log(array.length);
+
+
+        //  task 2
+
+
+// for (const item of array) {
+//     console.log(item);
+// }
+
+
+        //  task 3
+
+
+        // // for (const index in array) {
+        // //     console.log(index);
+        // }
+
+        
+        //  task 4
+
+
+        // console.log(Array.isArray(array)); 
+
+        //  task 5
+
+
+        //  const obj = { 0: "a", 1: "b", 2: "c", length: 3 };
+        // console.log(Array.from(obj)); 
+
+
+        //  task 6
+
+
+        // array.forEach(element => console.log(element));
+
+
+
+
+///////////////////////////  mid  ///////////////////////////////////////////
+       
+
+
+
+        //  task 1
+
+
+        // const arr1 = [1, 2, 3];
+
+        // const arr2 = [4, 5, 6];
+
+    //  console.log(arr1.concat(arr2)); 
+
+
+        //  task 2
+
+
+        // console.log(array.indexOf(3)); 
+
+
+        //  task 3
+
+
+        // console.log(array.slice(2, 4)); 
+
+        //  task 4
+
+
+//         array.splice(2, 1, 10); 
+// console.log(array); 
+
+
+        //  task 5
+
+
+        // console.log(array.join(", "));
+
+
+        //  task 6
+
+
+//         const str = "1,2,3,4,5";
+// console.log(str.split(",")); 
+
+
+        //  task 7
+
+
+        // console.log(Array.of(1, 2, 3)); 
+
+        //  task 8
+
+//         const arr = [1, 2, 3, 4, 5];
+// arr.fill(0, 0, 3);
+// console.log(arr); 
+
+
+
+        //  task 9 
+
+
+//         const arrCopy = [1, 2, 3, 4, 5];
+// arrCopy.copyWithin(1, 3);
+// console.log(arrCopy);
+
+
+        //  task 10
+        
+        
+//         const fruits = ["apple", "banana", "cherry"];
+// console.log(fruits.sort()); 
+
+
+
+       //  task 11
+
+
+    //    console.log(fruits.reverse()); 
+
+ ////////////////////advanced/////////////////////////////////////////
+      
+ 
+       //  task 1
+
+       const numbers = [1, 2, 3, 4, 5];
+    const squaredNumbers = numbers.map(num => num ** 2);
+    console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+    
+
+
+       //  task 2
+
+
+       const evens = array.filter(num => num % 2 === 0);
+       console.log(evens); // Output: [2, 4]
+       
+
+
+
+
